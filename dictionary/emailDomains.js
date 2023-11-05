b9da285e1b48d8162f1b3c395b26c279915504ea
mangadev <wagner.manganelli@taglivros.com.br>
2023-11-05 17:12:21 -0300

    support cjs and esm

--- dictionary/emailDomains.js
+++
@@ -1,1 +0,0 @@
-export const emailDomains = ['gmail', 'yahoo', 'outlook', 'live', 'hotmail'];
--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-import { firstNames } from './dictionary/firstNames.js';
-import { lastNames } from './dictionary/lastNames.js';
-import { emailDomains } from './dictionary/emailDomains.js';
-
-const getRandomInt = (max) => Math.floor(Math.random() * max);
-
-export const generateRandomFirstName = () => {
-    return firstNames[getRandomInt(firstNames.length)];
-};
-
-export const generateRandomLastName = () => {
-    return lastNames[getRandomInt(lastNames.length)];
-};
-
-export const generateRandomEmailDomain = () => {
-    return emailDomains[getRandomInt(emailDomains.length)];
-};
+module.exports = require('./lib/core/seedex.js');
